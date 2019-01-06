4ab63c5780fd283137d9760227e734d587e286ca
mb1011 <michaelbell1011@gmail.com>
2019-01-06 13:49:40 -0600

    saving work before starting on js logic

---
+++ js/data.js
@@ -0,0 +1,86 @@
+var data = [
+    {
+    "date": "1/6/2019",
+    "home_team": "Chicago Bulls",
+    "home_team_abr": "CHI",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/CHI.svg",
+    "location": "United Center",
+    "road_team": "Brooklyn Nets",
+    "road_team_abr": "BRK",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/BKN.svg",
+    "road_win_prediction": "Win",
+    "time": "3:30 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "Minnesota Timberwolves",
+    "home_team_abr": "MIN",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/MIN.svg",
+    "location": "Target Center",
+    "road_team": "Los Angeles Lakers",
+    "road_team_abr": "LAL",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/LAL.svg",
+    "road_win_prediction": "Win",
+    "time": "3:30 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "LA Clippers",
+    "home_team_abr": "LAC",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/LAC.svg",
+    "location": "Staples Center",
+    "road_team": "Orlando Magic",
+    "road_team_abr": "ORL",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/ORL.svg",
+    "road_win_prediction": "Loss",
+    "time": "3:30 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "Atlanta Hawks",
+    "home_team_abr": "ATL",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/ATL.svg",
+    "location": "State Farm Arena",
+    "road_team": "Miami Heat",
+    "road_team_abr": "MIA",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/MIA.svg",
+    "road_win_prediction": "Win",
+    "time": "6:00 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "Oklahoma City Thunder",
+    "home_team_abr": "OKC",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/OKC.svg",
+    "location": "Chesapeake Energy Arena",
+    "road_team": "Washington Wizards",
+    "road_team_abr": "WAS",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/WAS.svg",
+    "road_win_prediction": "Loss",
+    "time": "7:00 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "Toronto Raptors",
+    "home_team_abr": "TOR",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/TOR.svg",
+    "location": "Scotiabank Arena",
+    "road_team": "Indiana Pacers",
+    "road_team_abr": "IND",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/IND.svg",
+    "road_win_prediction": "Win",
+    "time": "7:30 PM"
+    },
+    {
+    "date": "1/6/2019",
+    "home_team": "Phoenix Suns",
+    "home_team_abr": "PHO",
+    "home_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/PHX.svg",
+    "location": "Talking Stick Resort Arena",
+    "road_team": "Charlotte Hornets",
+    "road_team_abr": "CHO",
+    "road_team_logo": "https://www.nba.com/assets/logos/teams/primary/web/CHA.svg",
+    "road_win_prediction": "Win",
+    "time": "8:00 PM"
+    }
+]
